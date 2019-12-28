a169348d7d6d53d0634d47981c8a53bdf21eadc1
Thanh Vuong <thanh@vuongs.org>
2019-12-28 01:02:08 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,62 @@
+import fetch from 'isomorphic-unfetch';
+import { parse, validate } from 'fast-xml-parser';
+import { location } from './location';
+
+export const feedUrl = 'https://dotfeeds.state.ma.us/api/RMVBranchWaitTime/Index';
+
+export const locationsMap = () => {
+  return location;
+};
+
+export const formatWaitTime = waitTime => {
+  if (waitTime === 'Closed') {
+    return -1;
+  }
+
+  try {
+    const timeSegment = waitTime.split(':');
+    if (!+timeSegment[0] && !+timeSegment[1] && !+timeSegment[2]) {
+      return 0;
+    }
+    const hour = +timeSegment[0] ? `${+timeSegment[0]}h ` : '';
+    const min = +timeSegment[1] ? `${+timeSegment[1]}m ` : '';
+    const sec = +timeSegment[2] ? `${+timeSegment[2]}s` : '';
+    const wait = `${hour}${min}${sec}`.trim();
+    return wait;
+  } catch (e) {
+    throw new Error(`Something broke. ${e}`);
+  }
+};
+
+export const parseWaitTimes = async () => {
+  const res = await fetch(feedUrl, {
+    method: 'GET',
+  });
+  const xml = await res.text();
+
+  if (!validate(xml)) {
+    throw new Error('XML is not valid');
+  }
+
+  const jsonObj = await parse(xml);
+
+  if (jsonObj.branches.branch.length < 1) {
+    throw new Error('No RMV data');
+  }
+
+  const branches = jsonObj.branches.branch.map(branch => {
+    if (location[branch.town]) {
+      const { address, lat, lon, phone } = location[branch.town];
+      branch.address = address;
+      branch.lat = lat;
+      branch.lon = lon;
+      branch.phone = phone;
+    }
+    return {
+      ...branch,
+      licensing: formatWaitTime(branch.licensing),
+      registration: formatWaitTime(branch.registration),
+    };
+  });
+  return branches;
+};
---
+++ tests/test.js
@@ -0,0 +1,34 @@
+import {
+  feedUrl,
+  formatWaitTime,
+  locationsMap,
+  parseWaitTimes,
+} from '../index';
+import { location } from '../location';
+
+test('feedUrl should be feed url', () => {
+  expect(feedUrl).toBe('https://dotfeeds.state.ma.us/api/RMVBranchWaitTime/Index');
+});
+
+test('locationsMap() should return locations map', () => {
+  expect(locationsMap()).toBe(location);
+});
+
+test('formatWaitTime() should handle Closed', () => {
+  expect(formatWaitTime('Closed')).toBe(-1);
+});
+
+test('formatWaitTime() should handle RMV wait time', () => {
+  expect(formatWaitTime('01:23:45')).toBe('1h 23m 45s');
+});
+
+test('formatWaitTime() should handle zero wait time', () => {
+  expect(formatWaitTime('00:00:00')).toBe(0);
+});
+
+test('parseWaitTimes() should return data', async () => {
+  expect.assertions(2);
+  const waitTimes = await parseWaitTimes();
+  expect(waitTimes.length).toBeGreaterThan(0);
+  expect(waitTimes[0].town).toBe('Attleboro');
+});
