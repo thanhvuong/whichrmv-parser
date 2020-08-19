4abf8f50a3e253be4f02c85b63fbbbbc62ee6fef
Thanh <thanh@vuongs.org>
2020-08-19 10:50:49 -0400

    fix

--- index.d.ts
+++ index.d.ts
@@ -1,4 +1,4 @@
-export function parseWaitTimes(): Promise<void>;
+export function parseWaitTimes(): Promise<any[]>;
 export function formatWaitTime(time: string): number | string;
 export function reformatWaitTime(waitTime: string): string;
 export function convertTimeSpanToString(timeSpan: number): string;
