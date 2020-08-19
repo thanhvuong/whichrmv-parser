d2438d4eba43c0b03c19155c394c9a768a93be22
Thanh <thanh@vuongs.org>
2020-08-19 01:33:02 -0400

    updates

--- index.js
+++ index.js
@@ -1,76 +1,87 @@
-import fetch from 'isomorphic-unfetch';
-import { parse, validate } from 'fast-xml-parser';
-import { location } from './location';
+import fetch from "isomorphic-unfetch";
+import { parse, validate } from "fast-xml-parser";
+import { location } from "./location";
+import { promisify } from "util";
 
-export const feedUrl = 'https://dotfeeds.state.ma.us/api/RMVBranchWaitTime/Index';
+const parseAsync = promisify(parse)
+
+export const feedUrl =
+  "https://dotfeeds.state.ma.us/api/RMVBranchWaitTime/Index";
 
 export const locationsMap = () => {
   return location;
 };
 
-export const formatWaitTime = waitTime => {
-  if (waitTime === 'Closed') {
+export const formatWaitTime = (waitTime) => {
+  if (waitTime === "Closed") {
     return -1;
   }
 
-  if (waitTime === 'Unavailable') {
+  if (waitTime === "Unavailable") {
     return -2;
   }
 
   try {
-    const timeSegment = waitTime.split(':');
+    const timeSegment = waitTime.split(":");
     if (!+timeSegment[0] && !+timeSegment[1] && !+timeSegment[2]) {
       return 0;
     }
-    const waitTimeInSeconds = (+timeSegment[0]) * 60 * 60 + (+timeSegment[1]) * 60 + (+timeSegment[2]); 
+    const waitTimeInSeconds =
+      +timeSegment[0] * 60 * 60 + +timeSegment[1] * 60 + +timeSegment[2];
     return waitTimeInSeconds;
   } catch (e) {
-    throw new Error('Error formatting wait time.', e);
+    throw new Error("Error formatting wait time.", e);
   }
 };
 
 export const reformatWaitTime = (waitTime) => {
   try {
-    const timeSegment = waitTime.split(':');
+    const timeSegment = waitTime.split(":");
     if (!+timeSegment[0] && !+timeSegment[1] && !+timeSegment[2]) {
       return 0;
     }
-    const hour = +timeSegment[0] ? `${+timeSegment[0]}h ` : '';
-    const min = +timeSegment[1] ? `${+timeSegment[1]}m ` : '';
-    const sec = +timeSegment[2] ? `${+timeSegment[2]}s` : '';
+    const hour = +timeSegment[0] ? `${+timeSegment[0]}h ` : "";
+    const min = +timeSegment[1] ? `${+timeSegment[1]}m ` : "";
+    const sec = +timeSegment[2] ? `${+timeSegment[2]}s` : "";
     const wait = `${hour}${min}${sec}`.trim();
     return wait;
-  } catch(e) {
-    throw new Error('Error reformatting wait time.', e)
+  } catch (e) {
+    throw new Error("Error reformatting wait time.", e);
   }
-}
+};
 
 export const convertTimeSpanToString = (timeSpan) => {
   try {
-    const convertedTimeSpan = new Date(1000 * timeSpan).toISOString().substr(11, 8);
+    const convertedTimeSpan = new Date(1000 * timeSpan)
+      .toISOString()
+      .substr(11, 8);
     return reformatWaitTime(convertedTimeSpan);
-  } catch(e) {
-    throw new Error('Error converting Timespan to string.', e)
+  } catch (e) {
+    throw new Error("Error converting Timespan to string.", e);
   }
-}
+};
+
+const parseXML = (xml) => new Promise((resolve, reject) => {});
 
 export const parseWaitTimes = async () => {
   const res = await fetch(feedUrl, {
-    method: 'GET',
+    method: "GET",
   });
   const xml = await res.text();
 
   if (!validate(xml)) {
-    throw new Error('XML is not valid');
+    throw new Error("XML is not valid");
   }
 
-  const jsonObj = await parse(xml);
+  const jsonObj = await parseAsync(xml);
 
-  if (jsonObj.branches.branch.length < 1) {
-    throw new Error('No RMV data');
+  const branchData = jsonObj?.branches?.branch ?? [];
+
+  if (branchData.length < 1) {
+    throw new Error("No RMV data");
   }
 
-  const branches = jsonObj.branches.branch.map(branch => {
+  const branches = branchData.map((branch) => {
     if (location[branch.town]) {
       const { address, lat, lon, phone } = location[branch.town];
       branch.address = address;
